4c627933225f1720f2df9e049c70aadfa4241987
kskkwn <kskkwn@gmail.com>
2020-05-06 11:13:39 +0900

    add package files

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spring-dtw",
+    version="0.0.1",
+    author="Keisuke Kawano",
+    author_email="kskkwn@gmail.com",
+    description="An implementation of the SPRING algorithm, which is a partial matching for time series with Dynamic Time Warping",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kskkwn/SPRING",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
