4308ade6b6ab3c342a1ef9c6421f4a556ec2ebe9
kskkwn <kskkwn@gmail.com>
2020-05-06 11:25:47 +0900

    update

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
 
 setuptools.setup(
     name="spring-dtw",
     version="0.0.1",
-    author="Keisuke Kawano",
-    author_email="kskkwn@gmail.com",
     description="An implementation of the SPRING algorithm, which is a partial matching for time series with Dynamic Time Warping",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    author="Keisuke Kawano",
     url="https://github.com/kskkwn/SPRING",
-    packages=["spring"],
+    packages=setuptools.find_packages("spring"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob('spring/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=_requires_from_file('requirements.txt'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.5',
 )
