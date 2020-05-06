0a9a4380686bd3d8bedcc9a086bc1e541d08bc5f
kskkwn <kskkwn@gmail.com>
2020-05-06 11:18:56 +0900

    update

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kskkwn/SPRING",
-    packages=setuptools.find_packages(),
+    packages=["spring"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: MIT License",
